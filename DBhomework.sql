afd31beb0c8f986f7c4b46c8378219a7219a0c7d
mr-Bratchik <157240542+mr-Bratchik@users.noreply.github.com>
2024-10-16 22:35:25 +0300

    Update and rename DBhomework.txt to DBhomework.sql

--- DBhomework.sql
+++ DBhomework.sql
@@ -1,25 +1,24 @@
-
 -- Жанры
-CREATE TABLE Genres(
+CREATE TABLE IF NOT EXISTS Genres(
 id SERIAL PRIMARY KEY,
 name VARCHAR(50) NOT NULL
 );
 
 -- Артисты
-CREATE TABLE Artists(
+CREATE TABLE IF NOT EXISTS Artists(
 id SERIAL PRIMARY KEY,
 name VARCHAR(50) NOT NULL 
 );
 
 -- Альбомы
-CREATE TABLE Albums(
+CREATE TABLE IF NOT EXISTS Albums(
 id SERIAL PRIMARY KEY,
 title VARCHAR(100) NOT NULL,
 release_year INT NOT NULL 
 );
 
 -- Песни
-CREATE TABLE Tracks(
+CREATE TABLE IF NOT EXISTS Tracks(
 id SERIAL PRIMARY KEY,
 title VARCHAR(100) NOT NULL,
 album_id INT REFERENCES Albums(id),
@@ -28,28 +27,28 @@ duration INTERVAL NOT NULL
 
 -- Сборники
 
-CREATE TABLE Collections(
+CREATE TABLE IF NOT EXISTS Collections(
 id SERIAL PRIMARY KEY,
 name VARCHAR(100) NOT NULL,
 release_year INT NOT NULL
 );
 
 -- Жанры артистов
-CREATE TABLE Artist_genres(
+CREATE TABLE IF NOT EXISTS Artist_genres(
 genre_id INT REFERENCES Genres(id),
 artist_id INT REFERENCES Artists(id),
 PRIMARY KEY (genre_id, artist_id)
 );
 
 -- Альбомы артистов
-CREATE TABLE Artist_albums(
+CREATE TABLE IF NOT EXISTS Artist_albums(
 album_id INT REFERENCES Albums(id),
 artist_id INT REFERENCES Artists(id),
 PRIMARY KEY (album_id, artist_id)
 );
 
 -- Песни сборников
-CREATE TABLE Collection_tracks(
+CREATE TABLE IF NOT EXISTS Collection_tracks(
 track_id INT REFERENCES Tracks(id),
 collection_id INT REFERENCES Collections(id),
 PRIMARY KEY (track_id, collection_id)
